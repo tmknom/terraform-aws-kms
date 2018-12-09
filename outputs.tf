32407748c3ce760dede0f4b0523f9681c31bb70c
tmknom <tmk.nom@gmail.com>
2018-12-09 23:01:01 +0900

    Implement KMS Key

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,9 @@
+output "kms_key_arn" {
+  value       = "${aws_kms_key.default.arn}"
+  description = "The Amazon Resource Name (ARN) of the key."
+}
 
+output "kms_key_id" {
+  value       = "${aws_kms_key.default.key_id}"
+  description = "The globally unique identifier for the key."
+}

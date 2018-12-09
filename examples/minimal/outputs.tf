37be7b85141bda0a5815e7eaf93ed5c9aebef778
tmknom <tmk.nom@gmail.com>
2018-12-09 23:10:26 +0900

    Implement minimal example

--- examples/minimal/outputs.tf
+++ examples/minimal/outputs.tf
@@ -1,1 +1,15 @@
+output "kms_key_arn" {
+  value = "${module.kms.kms_key_arn}"
+}
 
+output "kms_key_id" {
+  value = "${module.kms.kms_key_id}"
+}
+
+output "kms_alias_arn" {
+  value = "${module.kms.kms_alias_arn}"
+}
+
+output "kms_alias_target_key_arn" {
+  value = "${module.kms.kms_alias_target_key_arn}"
+}

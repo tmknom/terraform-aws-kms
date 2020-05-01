223282c6e5c0f213bd875a3763a914b910bcf46b
tmknom <tmk.nom@gmail.com>
2020-05-01 14:16:24 +0900

    Upgraded module to support Terraform 0.12

--- main.tf
+++ main.tf
@@ -4,17 +4,17 @@
 
 # https://www.terraform.io/docs/providers/aws/r/kms_key.html
 resource "aws_kms_key" "default" {
-  description = "${var.description}"
-  is_enabled  = "${var.is_enabled}"
+  description = var.description
+  is_enabled  = var.is_enabled
 
   # Only symmetric encryption and decryption are supported.
-  key_usage = "${var.key_usage}"
+  key_usage = var.key_usage
 
   # Key policies are the primary way to control access to customer master keys (CMKs) in AWS KMS.
   # They are not the only way to control access, but you cannot control access without them.
   # The default key policy gives the AWS account (root user) that owns the CMK full access to the CMK.
   # https://docs.aws.amazon.com/kms/latest/developerguide/key-policies.html
-  policy = "${var.policy}"
+  policy = var.policy
 
   # When you enable automatic key rotation for a customer managed CMK,
   # AWS KMS generates new cryptographic material for the CMK every year.
@@ -26,20 +26,20 @@ resource "aws_kms_key" "default" {
   # - After you enable key rotation, AWS KMS rotates the CMK automatically every year. You don't need to remember or schedule the update.
   #
   # https://docs.aws.amazon.com/kms/latest/developerguide/rotate-keys.html
-  enable_key_rotation = "${var.enable_key_rotation}"
+  enable_key_rotation = var.enable_key_rotation
 
   # Because it is destructive and potentially dangerous to delete a customer master key (CMK),
   # AWS KMS enforces a waiting period. To delete a CMK in AWS KMS you schedule key deletion.
   # You can set the waiting period from a minimum of 7 days up to a maximum of 30 days.
   # https://docs.aws.amazon.com/kms/latest/developerguide/deleting-keys.html#deleting-keys-how-it-works
-  deletion_window_in_days = "${var.deletion_window_in_days}"
+  deletion_window_in_days = var.deletion_window_in_days
 
   # A mapping of tags to assign to the resource.
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 # https://www.terraform.io/docs/providers/aws/r/kms_alias.html
 resource "aws_kms_alias" "default" {
   name          = "alias/${var.name}"
-  target_key_id = "${aws_kms_key.default.key_id}"
+  target_key_id = aws_kms_key.default.key_id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "kms_key_arn" {
-  value       = "${aws_kms_key.default.arn}"
+  value       = aws_kms_key.default.arn
   description = "The Amazon Resource Name (ARN) of the key."
 }
 
 output "kms_key_id" {
-  value       = "${aws_kms_key.default.key_id}"
+  value       = aws_kms_key.default.key_id
   description = "The globally unique identifier for the key."
 }
 
 output "kms_alias_arn" {
-  value       = "${aws_kms_alias.default.arn}"
+  value       = aws_kms_alias.default.arn
   description = "The Amazon Resource Name (ARN) of the key alias."
 }
 
 output "kms_alias_target_key_arn" {
-  value       = "${aws_kms_alias.default.target_key_arn}"
+  value       = aws_kms_alias.default.target_key_arn
   description = "The Amazon Resource Name (ARN) of the target key identifier."
 }
